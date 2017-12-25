7cf3d1329d7f65ec0e3675d4ce6f7054d8e76cfc
Gerald Versluis <gerald@verslu.is>
2017-12-25 18:25:48 +0100

    Initial commit

---
+++ LottieSample/LottieSamplePage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+
+namespace LottieSample
+{
+    public partial class LottieSamplePage : ContentPage
+    {
+        public LottieSamplePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            lottie.Play();
+        }
+
+        private void Handle1_Clicked(object sender, System.EventArgs e)
+        {
+            lottie.Pause();
+        }
+    }
+}
