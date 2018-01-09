739f503d49805483cc7f2f692a02484a855facd5
Alexandre Zollinger Chohfi <alzollin@microsoft.com>
2018-01-09 15:36:54 -0800

    Added UWP version.

---
+++ UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LottieSample.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            LoadApplication(new LottieSample.App());
+        }
+    }
+}
